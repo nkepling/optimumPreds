92958addf3ef689365f8fa13985f6a9c16380f3b
Nathaniel Keplinger <nkeplinger@mines.edu>
2022-04-27 14:05:52 -0600

    ar plots

---
+++ algorithmComparison.m
@@ -0,0 +1,20 @@
+%% create ar process
+A = [1 1.5 0.75];
+rng(123)
+x = filter(1,A,randn(1000,1));
+
+[H1,w1]=freqz(1,A);
+
+figure()
+periodogram(x)
+
+figure
+plot(w1/pi,20*log10(abs(H1)))
+xlabel("Normalized Frequency(\times \pi rad/sample")
+ylabel("Magnitude [dB]")
+title("Frequency response of AR process")
+
+
+
+
+
